18bfdaa9a4ab19b6526c32516a952e0d1d346e0b
Roman Kr <roma_as@list.ru>
2019-10-23 18:40:39 +0300

    Retrofit Calls and RxJava

--- app/src/main/java/com/mindyapps/android/dagger2mitch/network/auth/AuthApi.java
+++ app/src/main/java/com/mindyapps/android/dagger2mitch/network/auth/AuthApi.java
@@ -1,11 +1,14 @@
 package com.mindyapps.android.dagger2mitch.network.auth;
 
+import com.mindyapps.android.dagger2mitch.models.User;
+
+import io.reactivex.Flowable;
 import okhttp3.ResponseBody;
-import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface AuthApi {
 
-    @GET("fake")
-    Call<ResponseBody> getFakeStuff(); // just dummy method for now
+    @GET("users/{id}")
+    Flowable<User> getUser(@Path("id") int id);
 }
--- app/src/main/java/com/mindyapps/android/dagger2mitch/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/mindyapps/android/dagger2mitch/ui/auth/AuthViewModel.java
@@ -4,9 +4,14 @@ import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import com.mindyapps.android.dagger2mitch.models.User;
 import com.mindyapps.android.dagger2mitch.network.auth.AuthApi;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
 
@@ -19,5 +24,29 @@ public class AuthViewModel extends ViewModel {
         this.authApi = authApi;
         Log.d(TAG, "AuthViewModel: is working");
 
+        authApi.getUser(1)
+                .toObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                        Log.d(TAG, "onNext: " + user.getEmail());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
