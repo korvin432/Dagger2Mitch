7f14944662655461c18709791111ef742dd8818f
Roman Kr <roma_as@list.ru>
2019-10-23 18:59:44 +0300

    LiveData Reactive Streams and Flowables

--- app/src/main/java/com/mindyapps/android/dagger2mitch/ui/auth/AuthActivity.java
+++ app/src/main/java/com/mindyapps/android/dagger2mitch/ui/auth/AuthActivity.java
@@ -1,26 +1,33 @@
 package com.mindyapps.android.dagger2mitch.ui.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.bumptech.glide.RequestManager;
 import com.mindyapps.android.dagger2mitch.R;
+import com.mindyapps.android.dagger2mitch.models.User;
 import com.mindyapps.android.dagger2mitch.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class AuthActivity extends DaggerAppCompatActivity {
+public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener {
     private static final String TAG = "AuthActivity";
 
     private AuthViewModel viewModel;
+
+    private EditText userId;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -35,15 +42,47 @@ public class AuthActivity extends DaggerAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        userId = findViewById(R.id.user_id_input);
+
+        findViewById(R.id.login_button).setOnClickListener(this);
 
         viewModel = ViewModelProviders.of(this, providerFactory).get(AuthViewModel.class);
 
         setLogo();
+
+        subcribeObservers();
+    }
+
+    private void subcribeObservers(){
+        viewModel.observeUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                if (user != null){
+                    Log.d(TAG, "onChanged: " + user.getEmail());
+                }
+            }
+        });
     }
 
     private void setLogo(){
         requestManager
                 .load(logo)
                 .into((ImageView)findViewById(R.id.login_logo));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login_button:
+                attemptLogin();
+                break;
+        }
+    }
+
+    private void attemptLogin() {
+        if (TextUtils.isEmpty(userId.getText().toString())){
+            return;
+        }
+        viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()));
     }
 }
