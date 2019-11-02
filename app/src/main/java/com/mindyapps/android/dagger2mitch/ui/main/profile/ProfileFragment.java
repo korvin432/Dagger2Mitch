53030db7f4997a3928668fbcf750c3b289b30c1d
Roman Kr <roma_as@list.ru>
2019-11-02 18:35:21 +0300

    Dagger 2, Fragments and ViewModels

--- app/src/main/java/com/mindyapps/android/dagger2mitch/ui/main/profile/ProfileFragment.java
+++ app/src/main/java/com/mindyapps/android/dagger2mitch/ui/main/profile/ProfileFragment.java
@@ -5,13 +5,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.mindyapps.android.dagger2mitch.R;
+import com.mindyapps.android.dagger2mitch.models.User;
+import com.mindyapps.android.dagger2mitch.ui.auth.AuthResource;
 import com.mindyapps.android.dagger2mitch.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
@@ -19,10 +23,10 @@ import javax.inject.Inject;
 import dagger.android.support.DaggerFragment;
 
 public class ProfileFragment extends DaggerFragment {
-
     private static final String TAG = "ProfileFragment";
 
     private ProfileViewModel viewModel;
+    private TextView email, username, website;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -36,7 +40,62 @@ public class ProfileFragment extends DaggerFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onViewCreated: Profile fragment was created");
+        email = view.findViewById(R.id.email);
+        username = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
 
         viewModel = ViewModelProviders.of(this, providerFactory).get(ProfileViewModel.class);
+        subscribeObservers();
+    }
+
+    private void subscribeObservers(){
+        viewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+        viewModel.getAuthenticatedUser().observe(getViewLifecycleOwner(), new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if (userAuthResource != null){
+                    switch (userAuthResource.status){
+                        case AUTHENTICATED:{
+                            setUserDetails(userAuthResource.data);
+                            break;
+                        }
+                        case ERROR:{
+                            setErrorDetails(userAuthResource.message);
+                            break;
+                        }
+                    }
+                }
+
+            }
+        });
+    }
+
+    private void setErrorDetails(String message) {
+        email.setText(message);
+        username.setText("error");
+        website.setText("error");
+    }
+
+    private void setUserDetails(User data) {
+        email.setText(data.getEmail());
+        username.setText(data.getUsername());
+        website.setText(data.getWebsite());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/mindyapps/android/dagger2mitch/ui/main/profile/ProfileViewModel.java
+++ app/src/main/java/com/mindyapps/android/dagger2mitch/ui/main/profile/ProfileViewModel.java
@@ -2,7 +2,12 @@ package com.mindyapps.android.dagger2mitch.ui.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.mindyapps.android.dagger2mitch.SessionManager;
+import com.mindyapps.android.dagger2mitch.models.User;
+import com.mindyapps.android.dagger2mitch.ui.auth.AuthResource;
 
 import javax.inject.Inject;
 
@@ -10,8 +15,15 @@ public class ProfileViewModel extends ViewModel {
 
     private static final String TAG = "ProfileViewModel";
 
+    private final SessionManager sessionManager;
+
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         Log.d(TAG, "ProfileViewModel: viewmodel is ready");
+    }
+
+    public LiveData<AuthResource<User>> getAuthenticatedUser(){
+        return sessionManager.getAuthUser();
     }
 }
